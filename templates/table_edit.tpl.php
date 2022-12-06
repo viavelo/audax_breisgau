663d5525ca0e8924b44ebb1ae90ee6b12f4b5719
viavelo <77853013+viavelo@users.noreply.github.com>
2022-12-06 22:33:31 +0100

    Create table_edit.tpl.php

---
+++ templates/table_edit.tpl.php
@@ -0,0 +1,7 @@
+<html>
+    
+    <table class="table"><thead style="background: #ddd; "><td><b>Name</b></td><td><b>Vorname</b></td><td><b>Ventoux</b></td>
+    <td><b>Breisgau 1</b></td><td><b>Breisgau 2</b></td><td><b>Bölchen 1</b></td><td><b>Bölchen 2</b></td>
+    <td><b>Vogesen</b></td><td><b>Bodensee</b></td><td><b>Jura</b></td>
+    <td><b>M200</b></td><td><b>M300</b></td><td><b>M400</b></td><td><b>M600</b></td></thead>
+    <tbody>
