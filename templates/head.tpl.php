c424a187775a1e26ccef13cbb036a84fc7a6d617
viavelo <77853013+viavelo@users.noreply.github.com>
2022-12-06 22:30:52 +0100

    Update head.tpl.php

--- templates/head.tpl.php
+++ templates/head.tpl.php
@@ -5,8 +5,8 @@
     <link rel="stylesheet" type="text/css" href="themes/custom/audax_bootstrap/css/base/style.css">
     <style>
         .table {font-family: 'Helvetica Neue',Helvetica, Arial, sans-serif; border:1; table-layout:fixed;width:100%;}
-        .td_0, .td_3, .td_4, .td_7 {text-align: center; background-color: #ededed;}
-        .td_1, .td_2, .td_5, .td_6 {text-align: center; background-color: #fff;}
+        .td_0, .td_3, .td_4, .td_7 {text-align: center; background-color: #ededed; width: 100%;line-height: 1.8em;}
+        .td_1, .td_2, .td_5, .td_6 {text-align: center; background-color: #fff; width: 100%;line-height: 1.8em;}
         .td_8 {text-align: center; background-color: #58D256; width: 100%; line-height: 1.8em;}
     </style>
     <script type="text/javascript">
@@ -16,6 +16,7 @@
                 question = 'Datensatz löschen?'
             return window.confirm(question);
         }
+        history.replaceState(null, "", window.location.href); // Reload ohne POST ermöglichen
     </script>
     </style>
 </head>
