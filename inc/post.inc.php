1c2c8ef4ad72dd232ea609e468f22b99f2bb5a50
viavelo <77853013+viavelo@users.noreply.github.com>
2022-12-06 22:29:11 +0100

    Update post.inc.php

--- inc/post.inc.php
+++ inc/post.inc.php
@@ -1,4 +1,12 @@
 <?php
-    $ventoux = $_POST['ventoux'] ?? '';
-    $br1 = $_POST['br1'] ?? '';
+   $vname = $_POST['vname'] ?? '';
+   $nname = $_POST['nname'] ?? '';
+   $brevet = $_POST['brevet'] ?? '';
+   $tn_id = $_POST['tn_id'] ?? '';
+   $checked = $_POST['checked'] ?? '';
+   $delete = $_POST['delete'] ?? '';
+   $delete = $_POST['delete'] ?? '';
+
+   //$data = array($_POST['ventoux'],$_POST['br1'],$_POST['br2'],$_POST['boe1'],$_POST['boe2'],$_POST['vog'],$_POST['bod'], $_POST['jura']);
+   
     
