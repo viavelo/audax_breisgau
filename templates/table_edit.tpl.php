4efd8bdcf0f02c9771b585646dc2992d1233423b
viavelo <77853013+viavelo@users.noreply.github.com>
2023-01-02 13:25:52 +0100

    Update table_edit.tpl.php

--- templates/table_edit.tpl.php
+++ templates/table_edit.tpl.php
@@ -1,7 +1,12 @@
 <html>
-    
+    <div style="background: #b3cc3b; width: 200px; padding: 30px;margin: 30px; float: left;">
+        <a href="db_data_transfer.php"><b>DB-Aktualisierung</b></a>
+    </div>
+    <div style="background: #b3cc3b; width: 200px; padding: 30px;margin: 30px; float: right;">
+    <a href="csv_export.php"><b>CSV-Export</b></a>
+    </div>
     <table class="table"><thead style="background: #ddd; "><td><b>Name</b></td><td><b>Vorname</b></td><td><b>Ventoux</b></td>
     <td><b>Breisgau 1</b></td><td><b>Breisgau 2</b></td><td><b>Bölchen 1</b></td><td><b>Bölchen 2</b></td>
-    <td><b>Vogesen</b></td><td><b>Bodensee</b></td><td><b>Jura</b></td>
-    <td><b>M200</b></td><td><b>M300</b></td><td><b>M400</b></td><td><b>M600</b></td></thead>
+    <td><b>Bodensee</b></td><td><b>Vogesen</b></td><td><b>Jura</b></td>
+    <td><b>M200</b></td><td><b>M300</b></td><td><b>M400</b></td><td><b>M600</b></td><td><b>Comment</b></td></thead>
     <tbody>
