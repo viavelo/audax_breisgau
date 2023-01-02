f3f084c449b12ae67caebf71c812ef68c6818ae6
viavelo <77853013+viavelo@users.noreply.github.com>
2023-01-02 21:42:17 +0100

    Update funktionen.inc.php

--- inc/funktionen.inc.php
+++ inc/funktionen.inc.php
@@ -26,14 +26,4 @@ function list_tn($i, $get, $class)
       }
       return $x . '</td>';
 }
-//}
-function list_tn_br1($i)
-{   global $r;
-    $x =$r[$i]->getBr1();
-    if($x != 2) {$x = '<td class="td_0">&nbsp;';} 
-      else {
-      $x = '<td class="td_0">&#10004';
-      }
-      return $x . '</td><td></td></tr>';
-//echo '<tr><td>'. $r[$i]->getNname() . '</td><td>'. $r[$i]->getVname() . '</td><td>'. $r[$i]->getVentoux() . '</td><td>'. $r[$i]->getBr1()  . '</td><td>'. $r[$i]->getBr2(). '</td></tr>';
-}
+
