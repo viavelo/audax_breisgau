fe229e36a79f777619b82a954c45d926b5ab111b
viavelo <77853013+viavelo@users.noreply.github.com>
2023-01-02 11:37:37 +0100

    Update table_body_edit.tpl.php

--- templates/table_body_edit.tpl.php
+++ templates/table_body_edit.tpl.php
@@ -1,11 +1,11 @@
 <html>
     <tr id="<?= $r[$i]->getId(); ?>"><td  colspan = "2" style=""><form action="#<?= $r[$i]->getId(); ?>" method="POST">
     <input type="hidden" name="delete" value="<?= $r[$i]->getId(); ?>">
-    <button onclick="return ask_first(this, 'Teilnehmer löschen?');" class="td_0" style="line-height: 1.8em; margin-top: 16px;" type="submit">
-    <?= $r[$i]->getNname().' '.$r[$i]->getVname() ?></button></form></td>
+    <button onclick="return ask_first(this, 'Teilnehmer löschen?');" class="td_0" style="line-height: 1.8em; " type="submit">
+    <?= $r[$i]->getVname().' '.$r[$i]->getNname() ?></button></form></td>
     
     <form action="#<?= $r[$i]->getId(); ?>" method="POST"><input type="hidden" name="tn_id" value="<?= $r[$i]->getId(); ?>"><input type="hidden" name="brevet" value="ventoux">
-    <td><button type="submit" name="checked"
+    <td style="background: #fff;"><button type="submit" name="checked"
     <?= $r[$i]->showButtonVentoux(); ?>
     </button></td></form>
 
@@ -20,12 +20,12 @@
     </button></td></form>
 
     <form action="#<?= $r[$i]->getId(); ?>" method="POST"><input type="hidden" name="tn_id" value="<?= $r[$i]->getId(); ?>"><input type="hidden" name="brevet" value="boe1">
-    <td><button type="submit" name="checked"
+    <td style="background: #fff;"><button type="submit" name="checked"
     <?= $r[$i]->showButtonBoe1(); ?>
     </button></td></form>
   
     <form action="#<?= $r[$i]->getId(); ?>" method="POST"><input type="hidden" name="tn_id" value="<?= $r[$i]->getId(); ?>"><input type="hidden" name="brevet" value="boe2">
-    <td><button type="submit" name="checked"
+    <td style="background: #fff;"><button type="submit" name="checked"
     <?= $r[$i]->showButtonBoe2(); ?>
     </button></td></form>
   
@@ -40,27 +40,31 @@
     </button></td></form>
   
     <form action="#<?= $r[$i]->getId(); ?>" method="POST"><input type="hidden" name="tn_id" value="<?= $r[$i]->getId(); ?>"><input type="hidden" name="brevet" value="jura">
-    <td><button type="submit" name="checked"
+    <td style="background: #fff;"><button type="submit" name="checked"
     <?= $r[$i]->showButtonJura(); ?>
     </button></td></form>
 
     <form action="#<?= $r[$i]->getId(); ?>" method="POST"><input type="hidden" name="tn_id" value="<?= $r[$i]->getId(); ?>"><input type="hidden" name="brevet" value="med200">
-    <td><button type="submit" name="checked"
+    <td style="background:#cedf48;"><button type="submit" name="checked"
     <?= $r[$i]->showButtonMed200(); ?>
     </button></td></form>
 
     <form action="#<?= $r[$i]->getId(); ?>" method="POST"><input type="hidden" name="tn_id" value="<?= $r[$i]->getId(); ?>"><input type="hidden" name="brevet" value="med300">
-    <td><button type="submit" name="checked"
+    <td style="background:#cedf48;"><button type="submit" name="checked"
     <?= $r[$i]->showButtonMed300(); ?>
     </button></td></form>
 
     <form action="#<?= $r[$i]->getId(); ?>" method="POST"><input type="hidden" name="tn_id" value="<?= $r[$i]->getId(); ?>"><input type="hidden" name="brevet" value="med400">
-    <td><button type="submit" name="checked"
+    <td style="background:#cedf48;"><button type="submit" name="checked"
     <?= $r[$i]->showButtonMed400(); ?>
     </button></td></form>
 
     <form action="#<?= $r[$i]->getId(); ?>" method="POST"><input type="hidden" name="tn_id" value="<?= $r[$i]->getId(); ?>"><input type="hidden" name="brevet" value="med600">
-    <td><button type="submit" name="checked"
+    <td style="background:#cedf48;"><button type="submit" name="checked"
     <?= $r[$i]->showButtonMed600(); ?>
     </button></td></form>
+
+    <td style="background:#cedf48;"><p> 
+    <?= $r[$i]->getNachricht(); ?>
+    </p></td>
   
